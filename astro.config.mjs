5ec40a06ca5c678bd37963f03ffa963bc038a6f4
Balls <nicolajkorchikov54@gmail.com>
2023-05-23 15:26:16 +0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,16 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import react from "@astrojs/react";
 
-import astroI18next from "astro-i18next";
+// https://astro.build/config
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroI18next()],
+  integrations: [tailwind(), react()],
   defaultLocale: "en",
   locales: ["en", "fr"],
+  output: "server",
+  adapter: node({
+    mode: 'standalone'
+  })
 });
